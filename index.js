a1928fced64bb329f2eee05e6842d34bcf270d28
DaneVN <Dane.vN23@gmail.com>
2024-09-17 20:51:55 +0200

    Certificate/modal JS DONE

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ document.addEventListener('DOMContentLoaded', function () {
   
     cardForm.addEventListener('submit', function (e) {
       e.preventDefault();
-  
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput = document.getElementById('personalMessage');
+      const courseNameInput = document.getElementById('courseName'); 
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
@@ -25,9 +24,16 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
-      <h3>${studentName}</h3>
-    `;
+      certificateContent.innerHTML = `
+        <h2>Certificate of Achievement</h2>
+        <p>This is to certify that</p>
+        <h3>${studentName}</h3>
+        <p>has almost completed the</p>
+        <h4>${courseName} Course</h4>
+        <p>with legendary perseverence and world-class bad-assery for never giving up</p>
+        <img src="./logo.png">
+        <p>${personalMessage}</p>
+      `;
     
       //  Display the modal
       modal.style.display = 'block';
@@ -39,8 +45,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
+    closeModal.addEventListener('click', function close() {
+      modal.style.display = 'none'
     });
   });
   
